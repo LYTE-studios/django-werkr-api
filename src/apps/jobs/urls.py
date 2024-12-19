c99c3da11e2f9a6e7023e9d9698206cc1f3f3869
FutureSea <futuresea.dev@gmail.com>
2024-12-19 18:13:38 +0200

    open views

--- src/apps/jobs/urls.py
+++ src/apps/jobs/urls.py
@@ -1,25 +1,35 @@
 from django.urls import path
 from .views import (
-    JobView
+    JobView,
+    CreateJobView,
+    UpcomingJobsView,
+    AllUpcomingJobsView,
+    HistoryJobsView,
+    GetJobsBasedOnUserView,
+    TimeRegistrationView,
+    SignTimeRegistrationView,
+    ActiveJobList,
+    DoneJobList,
+    DraftJobList,
 )
 
 
 urlpatterns = [
     # Jobs
     path('details/<str:id>', JobView.as_view()),
-    # path('create', CreateJobView.as_view()),
-    # path('upcoming', UpcomingJobsView.as_view()),
-    # path('upcoming/all', AllUpcomingJobsView.as_view()),
-    # path('upcoming/all/<int:start>/<int:end>', AllUpcomingJobsView.as_view()),
-    # path('history', HistoryJobsView.as_view()),
-    # path('history/<int:start>/<int:end>', HistoryJobsView.as_view()),
-    # path('get', GetJobsBasedOnUserView.as_view()),
-    # path('timeregistration', TimeRegistrationView.as_view()),
-    # path('timeregistration/<str:user_id>', TimeRegistrationView.as_view()),
-    # path('timeregistration/sign', SignTimeRegistrationView.as_view()),
-    # path('active', ActiveJobList.as_view()),
-    # path('done', DoneJobList.as_view()),
-    # path('done/<int:start>/<int:end>', DoneJobList.as_view()),
-    # path('drafts', DraftJobList.as_view()),
+    path('create', CreateJobView.as_view()),
+    path('upcoming', UpcomingJobsView.as_view()),
+    path('upcoming/all', AllUpcomingJobsView.as_view()),
+    path('upcoming/all/<int:start>/<int:end>', AllUpcomingJobsView.as_view()),
+    path('history', HistoryJobsView.as_view()),
+    path('history/<int:start>/<int:end>', HistoryJobsView.as_view()),
+    path('get', GetJobsBasedOnUserView.as_view()),
+    path('timeregistration', TimeRegistrationView.as_view()),
+    path('timeregistration/<str:user_id>', TimeRegistrationView.as_view()),
+    path('timeregistration/sign', SignTimeRegistrationView.as_view()),
+    path('active', ActiveJobList.as_view()),
+    path('done', DoneJobList.as_view()),
+    path('done/<int:start>/<int:end>', DoneJobList.as_view()),
+    path('drafts', DraftJobList.as_view()),
 ]
 
