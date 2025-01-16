436bedcf21d5d718ae13502a9f10e007f96e5301
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2025-01-16 12:25:54 +0100

    Update base.py

--- src/api/settings/base.py
+++ src/api/settings/base.py
@@ -181,6 +181,10 @@ GOOGLE_ROUTES_URL = "https://routes.googleapis.com"
 
 DIMONA_URL = config('DIMONA_URL')
 DIMONA_AUTH_URL = config('DIMONA_AUTH_URL')
-EMPLOYER_DATA = config('EMPLOYER_DATA')
+
+EMPLOYER_DATA = {
+    "enterpriseNumber": config('ENTERPRISE_NUMBER'),
+} 
+
 DIMONA_CLIENT_ID = config('DIMONA_CLIENT_ID')
 JWT_SECRET = config('JWT_SECRET')
