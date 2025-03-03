da583f3b8e84ae962df544c5df69f2701981fa10
manong02 <manon.golos@gmail.com>
2025-03-03 15:46:24 +0100

    Update 0003_alter_storeddirections_from_lat_and_more.py
    added doc for: class Migration on 0003_alter_storeddirections_from_lat_and_more.py

--- src/apps/jobs/migrations/0003_alter_storeddirections_from_lat_and_more.py
+++ src/apps/jobs/migrations/0003_alter_storeddirections_from_lat_and_more.py
@@ -5,6 +5,16 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
+    """
+    Migration that alters an existing field in a model to the dabatase.
+    The altered fields are as follow:
+
+    - Altering 'from_lat', 'from_lon', 'to_lat', 'to_lon' fields in 'storeddirections' model by:
+    Changing the type of the field to 'FloatField'.
+    Necessary to allow the fields to store floating point numbers.
+
+    """
+
     dependencies = [
         ('jobs', '0002_alter_job_application_start_time_and_more'),
     ]
