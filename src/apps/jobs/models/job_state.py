86209d2ee1bfa8bb6ddffc84f9259168f825ac40
manong02 <manon.golos@gmail.com>
2025-03-03 18:00:26 +0100

    Update job_state.py
    added doc for: class JobState at job_state.py

--- src/apps/jobs/models/job_state.py
+++ src/apps/jobs/models/job_state.py
@@ -3,6 +3,22 @@ from django.db import models
 
 class JobState(models.TextChoices):
 
+    """
+    This class defines the possible states for a job using Django's TextChoices class.
+    
+    The states include:
+    - 'fulfilled': The job has been fulfilled.
+    - 'pending': The job is still under review.
+    - 'done': The job has been done.
+    - 'cancelled': The job has been cancelled.
+
+    Args:
+    models (TextChoices): Inherits from Django's TextChoices to define the states as text choices.
+
+    Returns:
+    None
+    """
+
     fulfilled = "fulfilled"
 
     pending = "pending"
