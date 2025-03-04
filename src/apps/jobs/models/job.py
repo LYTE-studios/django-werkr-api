b6c0cd4824ee3c54be817b2f52c859557c645f45
manong02 <manon.golos@gmail.com>
2025-03-04 09:28:11 +0100

    Update job.py
    added doc for job.py

--- src/apps/jobs/models/job.py
+++ src/apps/jobs/models/job.py
@@ -10,6 +10,15 @@ from django.utils import timezone
 
 
 class Job(models.Model):
+
+    """
+    This class manages operations related to jobs, such as determining if the job is currently
+    visible.
+    This model is associated with a specific id, customer, title, description, address,
+    keeping track of its state and the time within the application start and end time.
+    
+    """
+     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, default=None)
@@ -43,6 +52,24 @@ class Job(models.Model):
     selected_workers = models.IntegerField(null=True, blank=True)
 
     def is_visible(self):
+
+        """
+        Determines if the job is currently visible based on its application period, 
+        draft status, archive status, and worker availability.
+
+        A job is considered visible if:
+        - The current time is within the application start and end time.
+        - The job is not in draft mode.
+        - The job is not archived.
+        - The number of selected workers is less than the maximum allowed.
+
+        Args:
+        self (Job): An instance of the Job model.
+  
+        Returns:
+        bool: True if the job is visible, otherwise False.
+        """
+
         start = timezone.make_aware(self.application_start_time)
         end = timezone.make_aware(self.application_end_time)
 
