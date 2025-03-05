4e1edb327b7fc5b1800db3acaafbeee61aefcf3b
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2025-03-05 13:32:16 +0100

    fix: automatic fix group

--- src/apps/authentication/models/__init__.py
+++ src/apps/authentication/models/__init__.py
@@ -3,29 +3,9 @@ from django.db import models
 from django.conf import settings
 
 from .user import User
-from .custom_group import CustomGroup
 from .profiles.admin_profile import AdminProfile
 from .profiles.customer_profile import CustomerProfile
 from .profiles.worker_profile import WorkerProfile
 from .pass_reset import PassResetCode
 from .favorite_address import FavoriteAddress
 from .dashboard_flow import DashboardFlow, JobType, Location, WorkType, SituationType, UserJobType
-
-# Create the groups from the Django assumptions
-
-from apps.core.assumptions import *
-
-group, updated = Group.objects.update_or_create(name=CUSTOMERS_GROUP_NAME)
-
-if updated:
-    CustomGroup.objects.create(group=group, group_secret=settings.CUSTOMER_GROUP_SECRET)
-
-group, updated = Group.objects.update_or_create(name=WORKERS_GROUP_NAME)
-
-if updated:
-    CustomGroup.objects.create(group=group, group_secret=settings.WORKER_GROUP_SECRET)
-
-group, updated = Group.objects.update_or_create(name=CMS_GROUP_NAME)
-
-if updated:
-    CustomGroup.objects.create(group=group, group_secret=settings.CMS_GROUP_SECRET)
