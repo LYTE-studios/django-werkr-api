505b48030a92a31e2f04d43a6e9546374bcf11c1
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2025-05-13 11:11:48 +0200

    Update __init__.py

--- src/api/settings/__init__.py
+++ src/api/settings/__init__.py
@@ -4,8 +4,11 @@ from decouple import config
 ENV = config('DJANGO_ENV', default='staging')
 
 if ENV == 'production':
+    print('STARTED PRODUCTION')
     from .production import *
 elif ENV == 'staging':
+    print('STARTED STAGING')
     from .staging import *
 else:
+    print('STARTED DEVELOPMENT')
     from .development import *
